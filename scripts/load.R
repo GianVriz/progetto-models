1356272845c7203e822b349cc9d99564f4cbaa38
Daniele Zago - ST <zagodaniele.9@gmail.com>
2022-06-21 12:46:38 +0200

    Example
    - scripts/test.R
    Example of multi-armed bandit algorithms applied to the "men" dataset.
    
    - src/src.R
    Common functions for all scripts to use

--- scripts/load.R
+++
@@ -1,8 +0,0 @@
-# Source all files in /src
-for (f in list.files("src", pattern="*.R")) {
-    cat("f", "\n")
-    source(paste0("src/", f))
-}
-
-dat = read.csv(ffData("open_bandit_dataset", "random", "all", "all.csv"))
-
--- src/src.R
+++ src/src.R
@@ -1,6 +1,18 @@
-require("folderfun")
+### src.R: basic functions that all scripts will require
 
+# Define folders for data, plotting, source files, and scripts
+require("folderfun")
 setff("Data", "data/")
 setff("Plots", "plots/")
 setff("Src", "src/")
 setff("Scripts", "scripts/")
+
+# Install bandit R package
+require("devtools")
+devtools::install_github('Nth-iteration-labs/contextual')
+library(contextual)
+
+# Standardized plot dimensions
+save_png = function(filename, ...){
+    png(filename, width=1280, height=768, res=120, ...)
+}
